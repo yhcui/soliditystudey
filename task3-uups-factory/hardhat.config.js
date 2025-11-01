2c4a96a24eec1c8f1e01dadb697d830aa335560d
yhcui <cuiyuhui123@qq.com>
2025-11-01 22:46:42 +0800

    Update hardhat.config.js

--- task3-uups-factory/hardhat.config.js
+++ task3-uups-factory/hardhat.config.js
@@ -8,4 +8,11 @@ module.exports = {
   namedAccounts: {
     deployer: 0,
   },
+  networks: {
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/换成自己的",
+      accounts: ["小狐狸私钥"],
+      chainId: 11155111,
+    },
+  },
 };
